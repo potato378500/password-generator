0e8e2b5e4c7217af78f8d80568de14d5a5ac94a1
arwin <arwincyrus.iponla@gmail.com>
2026-07-18 19:14:49 +0800

    change onclick to just AddEventListener

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-let characters = [
-  "`",
-  "~",
-  "!",
-  "@",
-  "#",
+let characters = [ "`","~","!","@","#",
   "$",
   "%",
   "^",
@@ -79,33 +74,34 @@ let password2 = [];
 
 let password1_el = document.getElementById("pass1")
 let password2_el = document.getElementById("pass2")
+const main_btn = document.getElementById("main-btn")
 
-function generatePassword() {
+main_btn.addEventListener("click", function () { 
+  for (i = 0; i < 4; i++) {
+    let c1 = capital[Math.floor(Math.random() * capital.length + 1)];
+    let c2 = letters[Math.floor(Math.random() * letters.length + 1)];
+    let c3 = characters[Math.floor(Math.random() * characters.length + 1)];
+    let c4 = Math.floor(Math.floor(Math.random() * 9 + 1));
+
+    password1.push(c1, c2, c3, c4);
+  }
+
+  for (i = 0; i < 4; i++) {
+    let c1 = capital[Math.floor(Math.random() * capital.length + 1)];
+    let c2 = letters[Math.floor(Math.random() * letters.length + 1)];
+    let c3 = characters[Math.floor(Math.random() * characters.length + 1)];
+
+    password2.push(c1, c2, c3);
+  }
+
+  password1_el.value = password1.join("");
+  password2_el.value = password2.join("");
+
+
+
+
+})
      
-    let password1 = [];
-    let password2 = [];
-    
-    
-    for (i = 0; i < 4; i++) {
-      let c1 = capital[Math.floor(Math.random() * capital.length + 1)];
-      let c2 = letters[Math.floor(Math.random() * letters.length + 1)];
-      let c3 = characters[Math.floor(Math.random() * characters.length + 1)];
-      let c4 = Math.floor(Math.floor(Math.random()* 9 + 1));
-
-      password1.push(c1, c2, c3, c4);
-    }
-
-
-    for(i = 0 ; i<4; i++){
-             let c1 = capital[Math.floor(Math.random() * capital.length + 1)];
-             let c2 = letters[Math.floor(Math.random() * letters.length + 1)]; 
-             let c3 = characters[Math.floor(Math.random() * characters.length + 1)]; 
-            
-             password2.push(c1,c2,c3)
-             
-    }
 
     
-    password1_el.value = password1.join('');
-    password2_el.value = password2.join('');
-}
+
